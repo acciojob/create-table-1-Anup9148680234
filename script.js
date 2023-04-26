d70b7155d477abc39c076b9e5e6f89a18571869f
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-26 23:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  var tab = document.getElementById('sampleTable').innerHTML;
+  let x = `
+	  <tr><td>New Cell1</td> 
+	  <td>New Cell2</td></tr> 
+  ` + tab;
+	document.getElementById('sampleTable').innerHTML = x;
 }
